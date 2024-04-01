6891d14234a4d36243b8c320082c4833e560b719
MohamedUmar083 <umarmohd777@gmail.com>
2024-04-02 00:05:26 +0530

    Day-4/Tasks

--- script.js
+++ script.js
@@ -56,7 +56,7 @@
 //2.Convert all the strings to title caps in a string array
 
 /*
-//Annonymous Function
+//Annonymous Function:-
 const word = ["yOu kNoW wHo i aM"];
 var caps = function (word){
   var wordString = word.toString();
@@ -76,6 +76,7 @@ var caps = function (word){
 console.log(caps(word))
 */
 
+/*
 //IIFE Function:-
 const word = ["yOu kNoW wHo i aM"];
 (function (word){
@@ -92,8 +93,26 @@ const word = ["yOu kNoW wHo i aM"];
 
   console.log(total.join(" "));
 })(word)
+*/
 
+//Arrow Function:-
+const word = ["yOu kNoW wHo i aM"];
+var caps =  (word)=>{
+  var wordString = word.toString();
+  var lower = wordString.toLowerCase()
+  var split = lower.split(" ")
+  var total = []
+  for(var i=0;i<split.length;i++)
+  {
+    var capital = split[i].charAt(0).toUpperCase();
+    var rem = split[i].slice(1);
+    total.push(capital+rem);
+  }
 
+  return total.join(" ")
+}
+
+console.log(caps(word))
 
 
 
